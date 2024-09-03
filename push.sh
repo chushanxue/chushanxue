80d6b233d40eb2f4d4b7b2ce00fd974be048415c
chushanxue <2289350377@qq.com>
2024-09-03 16:22:04 +0800

    注释deploy文件

--- push.sh
+++ push.sh
@@ -3,6 +3,6 @@ cd chushanxue
 git config --global https.proxy 127.0.0.1:7891
 git config --global http.proxy 127.0.0.1:7891
 git add . 
-git commit -m "更改deploy文件为npm" --no-verify
+git commit -m "注释deploy文件" --no-verify
 # 只差git cz
 
