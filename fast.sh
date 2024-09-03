fa2217df213251af937e86ca1c42142a17e1926a
chushanxue <2289350377@qq.com>
2024-09-03 17:03:43 +0800

    静态文件路径适配本地

--- fast.sh
+++ fast.sh
@@ -3,6 +3,7 @@ cd WorkSpace
 cd chushanxue
 git config --global https.proxy 127.0.0.1:7891
 git config --global http.proxy 127.0.0.1:7891
+pnpm build:pre
 git add . 
 git commit -m "静态文件路径适配本地" --no-verify
 git push origin HEAD
