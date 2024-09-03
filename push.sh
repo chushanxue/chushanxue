0d81d4d74f2f3d475473221226996ce510d5d775
chushanxue <2289350377@qq.com>
2024-09-03 15:40:29 +0800

    提交deploy文件

--- push.sh
+++ push.sh
@@ -2,8 +2,7 @@ cd WorkSpace
 cd chushanxue
 git config --global https.proxy 127.0.0.1:7891
 git config --global http.proxy 127.0.0.1:7891
-pnpm build:pre
 git add . 
-git commit -m "更改为hash模式" --no-verify
+git commit -m "提交deploy文件" --no-verify
 # 只差git cz
 
